488152e38622418621f2258c46728a51c4607c0a
SatriaAgystaa <agystasatria@gmail.com>
2024-04-29 12:34:04 +0700

    add personal web

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        grey: '#0f0f0f',
+        tosca: '#10F5CC',
+        orange: '#ffab44',
+      },
+    },
+  },
+  plugins: [],
+}
